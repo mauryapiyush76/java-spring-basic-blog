088394ba29345929909eeacbba235b266c51f76e
mauryapiyush76 <47456936+mauryapiyush76@users.noreply.github.com>
2021-12-17 13:37:59 +0530

    task four

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pluralsight.blog.data.PostRepository;
@@ -24,5 +25,12 @@ public class BlogController {
 		List<Post> allPosts = postRepository.getAllPosts();
 		modelMap.put("posts", allPosts);
 		return "home";
+	}
+	
+	@RequestMapping("/post/{id}")
+	public String postDetails(@PathVariable Long id, ModelMap modelMap) {
+		Post post = postRepository.findById(id);
+		modelMap.put("post", post);
+		return "post-details";
 	}
 }
