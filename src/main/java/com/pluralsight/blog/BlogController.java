73405155323b6cb8f41e792a7ee53ae3b42fa4c7
mauryapiyush76 <47456936+mauryapiyush76@users.noreply.github.com>
2021-12-17 12:51:31 +0530

    second tasks

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,28 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
 @Controller
 public class BlogController {
 
+	private PostRepository postRepository;
+
+	public BlogController(PostRepository postRepository) {
+		super();
+		this.postRepository = postRepository;
+	}
+
 	@RequestMapping("/")
-	public String listPosts(ModelMap map) {
-		map.put("title", "Blog Post 1");
+	public String listPosts(ModelMap modelMap) {
+		List<Post> allPosts = postRepository.getAllPosts();
+		modelMap.put("posts", allPosts);
 		return "home";
 	}
 }
